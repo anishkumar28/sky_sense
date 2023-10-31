fcaed159f92295873e32616671e1f75f97e30fe4
Anish Kumar <anishkumar6527@gmail.com>
2023-10-31 19:03:28 +0530

    Remove the debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class SkySense extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: const LoadingScreen(),
     );
