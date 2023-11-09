a04133c4856ffe64b973dfa53accbc94c4054f29
Anish Kumar <anishkumar6527@gmail.com>
2023-11-09 23:32:35 +0530

    Restructuring code across all files

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sky_sense/screens/location_screen.dart';
-import 'package:sky_sense/services/location.dart';
-import 'package:sky_sense/services/networking.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:sky_sense/services/weather.dart';
 
-
-const apiKey = 'a3af5769a5a362782ac5eea804cac575';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -24,15 +21,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async{
-  Location location = Location();
 
-  await location.getCurrentLocation();
 
-  var url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-  NetworkHelper networkHelper = NetworkHelper(url.toString());
-
-  var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
   Navigator.push(context, MaterialPageRoute(builder: (context) {
     return LocationScreen(locationWeather: weatherData,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,25 @@
+import 'package:sky_sense/services/location.dart';
+import 'package:sky_sense/services/networking.dart';
+
+const apiKey = 'a3af5769a5a362782ac5eea804cac575';
+const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async{
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    var url = Uri.parse('$openWeatherMapUrl?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    NetworkHelper networkHelper = NetworkHelper(url.toString());
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
